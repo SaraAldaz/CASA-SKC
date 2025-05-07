909eb198aa012876bbf4804fd1bd9da3baf111bf
SaraAldaz <sarvaldazc124@umariana.edu.co>
2025-05-07 03:27:38 -0500

    Add files via upload

---
+++ Casa SKC/zapatos.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const productContainer = document.getElementById('productContainer')
+    fetch('https://humorous-perpetual-pipe.glitch.me/products')
+        .then(response => response.json())
+        .then(products => {
+            const filteredProducts = products.filter(product => product.category == "Shoes");
+            filteredProducts.forEach(product => {
+                const productCard = document.createElement('div');
+                productCard.classList.add('product-card');
+                productCard.innerHTML = `
+                <div class="product-image">
+                    <img src="${product.image}" alt="${product.title}">
+                </div>
+                <h2 class="product-title">${product.title}</h2>
+                <p class="product-price">${product.price}</p>
+                `;
+                productContainer.appendChild(productCard);
+            }); 
+        })
+        .catch(error => {
+            console.error('Error No Carga api correctamente', error);
+            productContainer.innerHTML = 'ERROR :No estan disponibles los productos';
+        });
+});
